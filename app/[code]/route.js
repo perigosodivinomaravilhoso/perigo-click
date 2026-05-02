67a01c20b2a10db19af2b5b8c43fc83127a72022
perigosodivinomaravilhoso <perigosodivinomaravilhoso@gmail.com>
2026-05-02 15:51:38 -0300

    Create route.js

---
+++ app/[code]/route.js
@@ -0,0 +1,16 @@
+import { supabase } from '@/lib/supabase';
+import { redirect } from 'next/navigation';
+
+export async function GET(req, { params }) {
+  const { data } = await supabase
+    .from('links')
+    .select('url')
+    .eq('code', params.code)
+    .single();
+
+  if (data?.url) {
+    redirect(data.url);
+  } else {
+    return new Response('Link não encontrado', { status: 404 });
+  }
+}
