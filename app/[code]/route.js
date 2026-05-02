59dc9fee3a2f1f9288df02034d743cc9c92c15f2
perigosodivinomaravilhoso <perigosodivinomaravilhoso@gmail.com>
2026-05-02 16:35:52 -0300

    Update route.js

--- app/[code]/route.js
+++ app/[code]/route.js
@@ -4,11 +4,17 @@ import { redirect } from 'next/navigation';
 export async function GET(req, { params }) {
   const { data } = await supabase
     .from('links')
-    .select('url')
+    .select('*')
     .eq('code', params.code)
     .single();
 
   if (data?.url) {
+    // incrementa clique
+    await supabase
+      .from('links')
+      .update({ clicks: data.clicks + 1 })
+      .eq('code', params.code);
+
     redirect(data.url);
   } else {
     return new Response('Link não encontrado', { status: 404 });
