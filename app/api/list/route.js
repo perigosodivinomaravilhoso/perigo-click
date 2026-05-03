1fc5e3e24aacf29fa9e0c3c5b26e893478d781bb
perigosodivinomaravilhoso <perigosodivinomaravilhoso@gmail.com>
2026-05-02 23:22:29 -0300

    Update route.js

--- app/api/list/route.js
+++ app/api/list/route.js
@@ -1,10 +1,24 @@
-import { supabase } from '../../../lib/supabase';
+import { createClient } from '../../../lib/supabase-server';
 
 export async function GET() {
-  const { data } = await supabase
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+
+  const { data, error } = await supabase
     .from('links')
     .select('*')
     .order('created_at', { ascending: false });
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
 
   return Response.json(data);
 }
